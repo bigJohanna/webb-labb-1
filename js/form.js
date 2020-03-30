d058eb55abb8ec1bdc100c7c618a7bab3c093101
Johanna <svallingson@gmail.com>
2020-03-30 11:37:39 +0200

    added jquery and js for templates

--- js/form.js
+++ js/form.js
@@ -1,20 +1,31 @@
+
+
 const elForm = document.getElementById('formReg');
 const elBoktipstitel = document.getElementById('boktipstitel');
 const elBoktipsBeskrivning = document.getElementById('boktipsbeskrivning');
 const elOutput = document.getElementById('output');
 
 function skickaTips(event){
-    // Använd value när man hämtar från input i formuläret
     const titel = elBoktipstitel.value;
     const boktipsBeskrivning = elBoktipsBeskrivning.value;
 
-    console.log("Textfält i funktion " + titel + boktipsBeskrivning);
+    validateForm();
+
+    console.log('Bokens titel "'  + titel + '" ' + 'Beskrivning "'  + boktipsBeskrivning +'"');
 
     elOutput.textContent = "Tack för tipset!";
 
     // Submittar inte formuläret
     event.preventDefault(); //
+    elForm.reset();
 }
 
 elForm.addEventListener('submit', skickaTips);
+
+function validateForm() {
+    if (elBoktipstitel.value === "") {
+        alert("Bokens titel måste fyllas i!");
+        return false;
+    }
+}
 
